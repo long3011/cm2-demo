4edecda87709915f0605248506845b02e9994c9e
long3011 <minhlongvohoang@gmail.com>
2025-09-26 11:16:10 +0300

    actually added auth

--- backend/api-server-starter/routes/jobRouter.js
+++ backend/api-server-starter/routes/jobRouter.js
@@ -5,6 +5,7 @@ const {
     updateJob,
     deleteJob
 } = require("../controllers/jobControllers");
+const {requireAuth} = require("../middleware/authMiddleware");
 const express = require("express");
 const router = express.Router();
 
@@ -12,15 +13,15 @@ const router = express.Router();
 router.get("/", getAllJobs);
 
 // POST /jobs - Create a new job
-router.post("/", createJob);
+router.post("/", requireAuth, createJob);
 
 // GET /jobs/:id - Retrieve a job by ID
 router.get("/:id", getJobById);
 
 // PUT /jobs/:id - Update a job by ID
-router.put("/:id", updateJob);
+router.put("/:id", requireAuth,  updateJob);
 
 // DELETE /jobs/:id - Delete a job by ID
-router.delete("/:id", deleteJob);
+router.delete("/:id", requireAuth, deleteJob);
 
 module.exports = router;
